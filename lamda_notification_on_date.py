da734c2bd79c53961854a065791f3f7fc103f0dc
marbo001 <mark.kosinski@gmail.com>
2018-07-31 10:13:50 -0500

    Add files via upload

---
+++ lamda_notification_on_date.py
@@ -0,0 +1,30 @@
+import boto3
+import re
+import datetime
+import requests
+
+ec = boto3.client('ec2')
+iam = boto3.client('iam')
+
+"""
+Lamda function to sent a webhook post, to slack for instance, to remind you that a date has been reached
+in order to clean up ec2 instances, or deprovision some other resources
+This is a work in progress...
+Derived from https://serverlesscode.com/post/lambda-schedule-ebs-snapshot-backups-2/
+
+"""
+
+def lambda_handler(event, context):
+    account_ids = '<AWS ACCOUNT ID>'
+
+    email_on = datetime.date.today().strftime('%Y-%m-%d')
+    filters = [
+        {'Name': 'tag-key', 'Values': ['EmailOn']},
+        {'Name': 'tag-value', 'Values': [email_on]},
+    ]
+    instance_response = ec.describe_instances(instance-id=account_ids, Filters=filters)
+
+    for inst in instace_response['Snapshots']:
+        print "Emailing %s" % inst['InstanceId']
+        payload={"text":"Today is the day!"}
+        r = requests.post('<WEBHOOK URL>', json=payload)
