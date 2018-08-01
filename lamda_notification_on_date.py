fda47774c690a7f5ed8739127e1c62270c8d4f5c
marbo001 <mark.kosinski@gmail.com>
2018-08-01 11:31:29 -0500

    Update lamda_notification_on_date.py
    massive changes.

--- lamda_notification_on_date.py
+++ lamda_notification_on_date.py
@@ -1,30 +1,39 @@
 import boto3
 import re
 import datetime
-import requests
+import json
+from botocore.vendored import requests
+from botocore.exceptions import ClientError
+from collections import defaultdict
 
 ec = boto3.client('ec2')
 iam = boto3.client('iam')
 
 """
-Lamda function to send a webhook post, to slack for instance, to remind you that a date has been reached
-in order to clean up ec2 instances, or deprovision some other resources
-This is a work in progress...
+Lamda function to sent a webhook post, to slack for instance, to remind you that a date has been reached on a resource
 Derived from https://serverlesscode.com/post/lambda-schedule-ebs-snapshot-backups-2/
+and
+https://www.slsmk.com/using-python-and-boto3-to-get-instance-tag-information/
 
+Set NotifyOn tag value on instance to YYYY-MM-DD format
 """
 
 def lambda_handler(event, context):
-    account_ids = '<AWS ACCOUNT ID>'
 
+    #filter instances that have a date tag equaling the current date
     notify_on = datetime.date.today().strftime('%Y-%m-%d')
     filters = [
         {'Name': 'tag-key', 'Values': ['NotifyOn']},
         {'Name': 'tag-value', 'Values': [notify_on]},
     ]
-    instance_response = ec.describe_instances(instance-id=account_ids, Filters=filters)
-
-    for inst in instace_response['Instances']:
-        print "Posting %s" % inst['InstanceId']
-        payload={"text":"Today is the day!"}
-        r = requests.post('<WEBHOOK URL>', json=payload)
+    #collect instances 
+    instance_response = ec.describe_instances(Filters=filters)
+   
+    instancelist = []
+    
+    for reservation in (instance_response["Reservations"]):
+        for instance in reservation["Instances"]:
+            instancelist.append(instance["InstanceId"])
+   #send json payload to webhook
+    payload={"text":"Time to review: " + json.dumps(instancelist)}
+    r = requests.post('<WEBHOOK URL>', json=payload)
